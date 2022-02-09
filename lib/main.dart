1b179fa04a8d92c036258260cb8abfeae73172cf
Carlos Eduardo <cadu@caduandrade.net>
2022-02-09 14:11:05 -0300

    Updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:demoflu/demoflu.dart';
+import 'package:easy_table_demo/custom_cell.dart';
 import 'package:easy_table_demo/get_started.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   Size? maxSize;
-  //maxSize = Size(400, 300);
+  // maxSize = const Size(400, 200);
   runApp(DemoFluApp(
       title: 'EasyTable (0.1.0)',
       widgetBackground: Colors.white,
@@ -16,7 +17,13 @@ void main() {
               builder: () => GetStartedExample(),
               codeFile: 'lib/get_started.dart',
               maxSize: maxSize,
-              indentation: 2)
+              indentation: 1),
+          MenuItem(
+              name: 'Custom cell',
+              builder: () => CustomCellExample(),
+              codeFile: 'lib/custom_cell.dart',
+              maxSize: maxSize,
+              indentation: 1)
         ];
       }));
 }
