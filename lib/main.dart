01a46e5b963a87fdc557e21eb793b7acf4713a5a
Carlos Eduardo <cadu@caduandrade.net>
2022-05-13 17:40:45 -0300

    Version 0.14.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:demoflu/demoflu.dart';
+import 'package:demoflu/demoflu.dart' as demo;
 import 'package:easy_table_demo/columns_fit.dart';
 import 'package:easy_table_demo/custom_cell.dart';
 import 'package:easy_table_demo/get_started.dart';
@@ -10,45 +10,45 @@ import 'package:flutter/material.dart';
 void main() {
   Size? maxSize;
   // maxSize = const Size(400, 201);
-  runApp(DemoFluApp(
-      title: 'EasyTable (1.2.0)',
+  runApp(demo.DemoFluApp(
+      title: 'EasyTable (1.3.0)',
       widgetBackground: Colors.white,
       resizable: true,
       appMenuBuilder: () {
         return [
-          MenuItem(
+          demo.MenuItem(
               name: 'Get started',
               builder: () => GetStartedExample(),
               codeFile: 'lib/get_started.dart',
               maxSize: maxSize,
               indentation: 1),
-          MenuItem(
+          demo.MenuItem(
               name: 'Columns fit',
               builder: () => ColumnsFitExample(),
               codeFile: 'lib/columns_fit.dart',
               maxSize: maxSize,
               indentation: 1),
-          MenuItem(
+          demo.MenuItem(
               name: 'Custom cell',
               builder: () => CustomCellExample(),
               codeFile: 'lib/custom_cell.dart',
               maxSize: maxSize,
               indentation: 1),
-          MenuItem(
+          demo.MenuItem(
               name: 'Row callbacks',
               builder: () => RowCallbacksExample(),
               codeFile: 'lib/row_callbacks.dart',
               maxSize: maxSize,
               consoleEnabled: true,
               indentation: 1),
-          MenuItem(name: 'Null values', indentation: 1),
-          MenuItem(
+          demo.MenuItem(name: 'Null values', indentation: 1),
+          demo.MenuItem(
               name: 'Cell color',
               builder: () => NullCellColorExample(),
               codeFile: 'lib/null_cell_color.dart',
               maxSize: maxSize,
               indentation: 2),
-          MenuItem(
+          demo.MenuItem(
               name: 'Pinned column',
               builder: () => PinnedColumnExample(),
               codeFile: 'lib/pinned_column.dart',
